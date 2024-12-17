ba38746071cc1e04a513836c6200bf29b52683bf
Flahz <mik.lapierre07@gmail.com>
2024-12-17 17:16:00 -0500

    dwad

--- cours8-ajax/httpbin.js
+++ cours8-ajax/httpbin.js
@@ -4,8 +4,3 @@ function handleErrors(response) {
     return response;
 }
 
-fetch("https://httpbin.org/post", {method: "POST", body: "GTI525"})
-    .then( handleErrors )
-    .then( response => response.json() )
-    .then( data => console.log( data.data) )
-    .catch( error => console.log( "Une erreur est survenue: " + error ));
